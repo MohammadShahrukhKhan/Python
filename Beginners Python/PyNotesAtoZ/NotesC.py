b1c79905b2d1e299d98418cd0aa6567c9f9cd62c
Mohammad Shahrukh Khan <102281342+MohammadShahrukhKhan@users.noreply.github.com>
2023-07-28 06:11:54 +0530

    Update NotesC.py

--- Beginners Python/PyNotesAtoZ/NotesC.py
+++ Beginners Python/PyNotesAtoZ/NotesC.py
@@ -2,7 +2,6 @@
 sense = input('Write your sentence')
 print(sense)
 
-oyo = input('Enter the word to replace')
-taj_hotel = input('Enter the word to replace it with')
-print(sense.replace(oyo , taj_hotel)) 
-
+honey = input('Enter the word to replace')
+milk = input('Enter the word to replace it with')
+print(sense.replace(honey, milk)) 
